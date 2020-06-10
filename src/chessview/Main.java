c113873b4e6c2baf7703eafdf2f164d9d44c97f6
chudau <tien.dd176888@sis.hust.edu.vn>
2020-06-10 12:59:13 +0700

    fix some errors

--- src/chessview/Main.java
+++ src/chessview/Main.java
@@ -10,7 +10,7 @@ import chessview.viewer.*;
  * @author hung
  */
 public class Main {
-	
+
 	/**
 	 * The main method.
 	 *
@@ -19,52 +19,48 @@ public class Main {
 	public static void main(String[] args) {
 		var path = "";
 		try {
-			if (args.length > 0) {
-				path = args[0];
-			} else {
-				path = "/home/ductiendo/Workspaces/ICT/3/OOP_Lab/sol/ChessView/test-case.txt";
-			}
-			ChessGame game = new ChessGame(new FileReader(path));			
+			path = args[0];
+			ChessGame game = new ChessGame(new FileReader(path));
 			new BoardFrame(game);
-			//textView(game);
-		} catch(IOException e) {			
+			// textView(game);
+		} catch (IOException e) {
 			System.err.println("Error loading file: " + path);
-			System.err.println(e.getMessage());			
+			System.err.println(e.getMessage());
 		}
 	}
-	
+
 	/**
 	 * Text view.
 	 *
-	 * @param game 
+	 * @param game
 	 */
 	public static void textView(ChessGame game) {
-		try {			
+		try {
 			List<Board> boards = game.boards();
 			List<Round> rounds = game.rounds();
-			
+
 			System.out.println(boards.get(0));
-			
-			for(int i=0;i!=rounds.size();++i) {
-				System.out.println("\n==================");	
+
+			for (int i = 0; i != rounds.size(); ++i) {
+				System.out.println("\n==================");
 				Round r = rounds.get(i);
-				System.out.println("WHITE PLAYS: " + r.white());				
-				if(((2*i)+1) >= boards.size()) {
+				System.out.println("WHITE PLAYS: " + r.white());
+				if (((2 * i) + 1) >= boards.size()) {
 					throw new RuntimeException("Invalid move!");
 				}
-				Board wb = boards.get((2*i)+1);																			
+				Board wb = boards.get((2 * i) + 1);
 				System.out.println(wb);
-				if(r.black() != null) {
+				if (r.black() != null) {
 					System.out.println("\nBLACK PLAYS: " + r.black());
-					if(((2*i)+2) >= boards.size()) {
+					if (((2 * i) + 2) >= boards.size()) {
 						throw new RuntimeException("Invalid move!");
 					}
-					Board bb = boards.get((2*i)+2);
+					Board bb = boards.get((2 * i) + 2);
 					System.out.println(bb);
-				}				
-			}			
-			
-		} catch(Exception e) {			
+				}
+			}
+
+		} catch (Exception e) {
 			e.printStackTrace(); // useful for debugging
 		}
 	}
