908f4bab8b594d72e02e111588ae8231046036b2
chudau <tien.dd176888@sis.hust.edu.vn>
2020-06-10 13:11:22 +0700

    add invalid pawnmoves test cases

--- src/tests/part2/InvalidPawnTests.java
+++ src/tests/part2/InvalidPawnTests.java
@@ -19,7 +19,13 @@ public class InvalidPawnTests extends TestCase {
 			"e2-e4 e7-e5\ne4-d5",
 			"e2-e4 e7-e5\ne4-e3",
 			"a2-a3 d7-d5\na3-a4 d5-d4\na4-a5 d4-d3\nd2-d4",
-			"a2-a3 d7-d5\na3-a4 d5-d4\nd2-d4"
+			"a2-a3 d7-d5\na3-a4 d5-d4\nd2-d4",
+
+			//additional test cases
+			"e2-d3",
+			"e2-e3 e7-d6",
+			"e2-e4 e7-e6\ne4-d4",
+			"e2-e4 e7-e6\nd2-d4 e6-d6",
 		};
 		checkInvalidTests(tests);
 	}
